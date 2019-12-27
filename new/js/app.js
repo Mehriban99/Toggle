f5de585cdf390c1b7b4bb6584ed62fe86f5280ca
Mehriban99 <57294601+Mehriban99@users.noreply.github.com>
2019-12-28 00:18:27 +0400

    Add files via upload

---
+++ new/js/app.js
@@ -0,0 +1,29 @@
+function MyFunction () {
+    var element = document.getElementById("alt");
+    if(element.style.display == "none") {
+        element.style.display = "block"
+    }
+    else{
+        element.style.display = "none"
+    }
+}
+
+function tabMenu() {
+    var slidebar = document.getElementById("slidebarr");
+    if(slidebar.style.display == "none") {
+        slidebar.style.display = "block"
+    }
+    else{
+        slidebar.style.display = "none"
+    }
+}
+
+function MyMedia() {
+    var sign = document.getElementById("mediaalt");      
+    if(sign.style.display == "none") {
+        sign.style.display = "block"
+    }
+    else{
+        sign.style.display = "none"
+    }
+}
